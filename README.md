cd2eea849d447d494fd27135695cf26289fe00ff
Tim Cardwell <Timothy.cardwell@gmail.com>
2018-10-14 21:38:12 -0500

    feat(api-integration): initial stab at retrieving task templates

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
 npm install vss-web-extension-sdk --save // should this be installed globally?
 need typescript@2.9.2 installed globally for cli
 need tfx-cli@0.6.3 installed globally for cli
+
+
+Update styles according to this: https://docs.microsoft.com/en-us/azure/devops/extend/develop/styles-from-widget-sdk?view=vsts
+
+
+Marketplace: https://marketplace.visualstudio.com/manage/publishers/TimCardwell
--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,79 @@
+import _WorkClient = require("TFS/Work/RestClient");
+import _WorkItemClient = require("TFS/WorkItemTracking/RestClient");
+import _WorkItemService = require("TFS/WorkItemTracking/Services");
+
 export function Log(): void {
   console.log("Hooked up correctly!")
+
+  var context = VSS.getWebContext();
+  console.log(context);
+
+  GetWorkItemService(context)
+    .then(result => {
+      console.log(result);
+    });
+
+  const workClient = GetWorkClient();
+  //workClient.getBoard()
+  //workClient.getTeamSettings(context.team.id)  
+
+  const workItemClient = GetWorkItemClient();
+
+  console.log("Getting relation types...")
+  workItemClient.getRelationTypes()
+    .then(result => {
+      console.log("Successfully got relation types");
+      console.log(result);
+    });
+
+  console.log("Getting root nodes...")
+  workItemClient.getRootNodes(context.project.id)
+    .then(result => {
+      console.log("Successfully got root nodes");
+      console.log(result);
+    });
+
+  console.log("Getting templates...")
+  workItemClient.getTemplates(context.project.id, context.team.id)
+    .then(result => {
+      console.log("Successfully got templates");
+      console.log(result);
+
+      const taskSuiteTemplates = result.filter(x => x.workItemTypeName == "Task");
+    });
+
+  /*
+  console.log("Getting work items...")
+  workItemClient.getWorkItems()
+    .then(result => {
+      console.log("Successfully got root nodes");
+      console.log(result);
+      for (const workItemClassificationNode in result) {
+        console.log(workItemClassificationNode);
+      }
+    });
+  */
+
+  console.log("Getting work item type categories...")
+  workItemClient.getWorkItemTypeCategories(context.project.id)
+    .then(result => {
+      console.log("Successfully work item type categories");
+      console.log(result);
+    });
+
+  console.log("Getting work item types...")
+  workItemClient.getWorkItemTypes(context.project.id)
+    .then(result => {
+      console.log("Successfully got work item types");
+      console.log(result);
+      result.forEach(workItemType => {
+        if (workItemType.name != 'User Story') {
+          // TODO Don't show this group!
+        } else {
+          // TODO Show this group!
+        }
+      });
+    });
 }
 
 export function Initialize(): void {
@@ -20,6 +94,18 @@ export function Execute(taskId: any): void {
 export function OnChange(evt: any): void {
   // Call other method
   throw Error("Not Implemented");
+}
+
+function GetWorkClient(): _WorkClient.WorkHttpClient4_1 {
+  return _WorkClient.getClient();
+}
+
+function GetWorkItemService(context: WebContext): IPromise<_WorkItemService.IWorkItemFormService> {
+  return _WorkItemService.WorkItemFormService.getService(context);
+}
+
+function GetWorkItemClient(): _WorkItemClient.WorkItemTrackingHttpClient4_1 {
+  return _WorkItemClient.getClient()
 }
 
 function UpdateSelectedTemplate(): void {
